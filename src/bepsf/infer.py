fix ePSF normalization

src/bepsf/infer.py
@@ -10,18 +10,25 @@ from numpyro.infer import init_to_value
 
 def optimize_flux_and_position(gridpsf, image_obs, image_err,
                                lnfluxes_guess, xcenters_guess, ycenters_guess, idx_anchor, method="TNC", n_iter=1, radius=3.,
-                               lnfluxlim=[-10.,2.], xyclim=[-2.,2.], lnlenxlim=[0,0], lnlenylim=[0,0], lnamplim=[-5.,0.]):
+                               lnfluxlim=[-10.,2.], xyclim=[-2.,2.], lnlenxlim=[0,0], lnlenylim=[0,0], lnamplim=[-3.,3.]):
 
     #Zanchor_mean = np.mean(image_obs.Z[image_obs.aperture_flux(xcenters_guess[idx_anchor], ycenters_guess[idx_anchor], 3.)])
-    flux_ap = image_obs.Z[image_obs.aperture_flux(xcenters_guess[idx_anchor], ycenters_guess[idx_anchor], radius)]
-    dfmedian = np.abs(np.median(np.diff(flux_ap)))
+    
+    #flux_ap = image_obs.Z[image_obs.aperture_flux(xcenters_guess[idx_anchor], ycenters_guess[idx_anchor], radius)]
+    #dfmedian = np.abs(np.median(np.diff(flux_ap)))
+    #lna_guess = np.log(dfmedian)
+    
+    Npix = (gridpsf.xgrid_edge.max()-gridpsf.xgrid_edge.min())*(gridpsf.ygrid_edge.max()-gridpsf.ygrid_edge.min())
+    lna_guess = -np.log(Npix)
+    print ("lna_guess:", lna_guess)
+    
     p_init = {
         "lnfluxes": lnfluxes_guess,
         "xcenters": xcenters_guess,
         "ycenters": ycenters_guess,
-        "lnlenx": np.float64(0.+0.5), # required so that pytree leaf has "shape"
-        "lnleny": np.float64(0.+0.5),
-        "lnamp": np.float64(np.log(dfmedian))
+        "lnlenx": np.float64(0.5), # np.float64() required so that pytree leaf has "shape"
+        "lnleny": np.float64(0.5),
+        "lnamp": np.float64(lna_guess)
     }
        
     p_low, p_high = {}, {}